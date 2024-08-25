2eb4051dd18ba8e60bb537827f5c95ad39055c49
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-24 23:10:25 -0700

    more caching

---
+++ index.py
@@ -0,0 +1,62 @@
+import numpy as np
+from pet_exp_calc import pet_level_to_exp
+from colors import bcolors
+import json
+import re
+
+rarities = ["COMMON", "UNCOMMON", "RARE", "EPIC", "LEGENDARY"]
+
+all_margin = []
+
+with open('cache/pet_cache.json', 'r') as openfile:
+    auc_list = json.load(openfile)["auctions"]
+with open('cache/pet_list.json', 'r') as openfile:
+    pet_list = json.load(openfile)["pet_list"]
+with open('cache/pet_blacklist.json', 'r') as openfile:
+    pet_blacklist = json.load(openfile)["pets"]
+
+print(bcolors.OKCYAN + "Auction Number: " + str(len(auc_list)) + bcolors.ENDC)
+for pet_name in pet_list:
+    if pet_name in pet_blacklist:
+        continue
+    for rarity in rarities:
+        pet_margins = []
+        pet_prices = []
+
+        low_tier_min = float('inf')
+        low_tier_min_lvl = 100
+        max_tier_min = float('inf')
+        max_tier_min_lvl = 0
+
+        for auc in auc_list:
+            if auc["item_name"].endswith(pet_name) and auc["tier"] == rarity and auc["bin"] == True:
+                price = auc["starting_bid"]
+                
+                
+                
+                # gets the pet level then removes "] " after the pet level to account for [Level 1] to [Level 100]
+                pet_level = int(re.sub("[^0-9]", "", auc["item_name"]))
+                
+                # gets current exp
+                if pet_level != 100:
+                    exp = auc["item_lore"][(auc["item_lore"].find("§l§m §r §e") + 10):-1]
+                    exp = exp[0:(exp.find("§6/"))]
+                    exp = exp.replace(",", "")
+                    try:
+                        exp = float(exp)
+                    except:
+                        continue
+                else: exp = 0
+                
+                
+                if pet_level > max_tier_min_lvl:
+                    max_tier_min = price
+                    max_tier_min_lvl = pet_level
+                elif pet_level < low_tier_min_lvl:
+                    low_tier_min = price
+                    low_tier_min_lvl = pet_level
+                elif pet_level == 100:
+                    max_tier_min = min(max_tier_min, price)
+                elif pet_level == 1:
+                    low_tier_min = min(low_tier_min, price)
+          
