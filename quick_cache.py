68b21adb1f6bf55893006f50abd1200b13d71bd5
Towster_ <79161644+TowsterBusiness@users.noreply.github.com>
2024-08-24 23:13:11 -0700

    quick cache

--- quick_cache.py
+++ quick_cache.py
@@ -18,3 +18,30 @@ def add_auction(pN):
     global pageNum
     global pet_list
     global pet_auction_list
+    
+   
+    result = req.get(url + '?page=' + str(pN)).json()
+    
+    for auc in result["auctions"]:
+        if auc["bin"]:
+            auction_list.append(auc)
+            if (auc["item_name"][auc["item_name"].find("]") + 2:] in pet_list):
+                pet_auction_list.append(auc)
+        
+    pages_completed += 1
+    # Checks if all pages were checked
+    if (pages_completed >= pageNum):
+        # Writes to the cache.json file with the full auctions list
+        dic = {
+            "auctions": auction_list
+        } 
+
+        json_object = json.dumps(dic, indent=4)
+        
+        with open("cache/cache.json", "w") as outfile:
+            outfile.write(json_object)
+            
+        # Writes to the pet_chache.json with only the pet auctions list
+        dic = {
+            "auctions": pet_auction_list
+        } 
